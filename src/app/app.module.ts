3762a7032066d7a4192ee1277d9cae9b4e83d3c0
SDevM <lolicongress@outlook.com>
2023-07-03 01:35:41 -0500

    Screensaver added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScreensaverComponent } from './components/screensaver/screensaver.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreensaverComponent
   ],
   imports: [
     BrowserModule,
